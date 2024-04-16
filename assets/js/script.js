f461cc1fad77a89ac29215b1cdac1aae86378b50
Vinay <vinaywebguy@gmail.com>
2024-04-16 15:52:45 +0530

    All front pages added

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+$('.faq-ques').click(function () {
+    $('.single-faq').removeClass('open')
+    let parent = $(this).parent().toggleClass('open');
+
+})
+
+
+$(document).ready(function () {
+    $('.view-password').click(function () {
+        let passwordInput = $('#password');
+        let passType = passwordInput.attr('type');
+
+        if (passType === 'password') {
+            passwordInput.attr('type', 'text');
+            $(this).removeClass('fa-eye').addClass('fa-eye-slash');
+        } else {
+            passwordInput.attr('type', 'password');
+            $(this).removeClass('fa-eye-slash').addClass('fa-eye');
+        }
+    });
+});
